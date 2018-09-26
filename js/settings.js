737c34aff3b9676091f0be3076f271d5a845c591
teranchristian <chris.teran@gmail.com>
2018-09-26 23:26:12 +1000

    WIP add storage

--- js/settings.js
+++ js/settings.js
@@ -1,33 +1,37 @@
+var data = {
+  tangos: {}
+};
 
 function save_options() {
-  var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
-  chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
-  }, function() {
-    // Update status to let user know options were saved.
+  var tango = document.getElementById('tango').value;
+  var hiragana = document.getElementById('hiragana').value;
+  var katakana = document.getElementById('katakana').value;
+  var displayDropdown = document.getElementById("display");
+  var display = displayDropdown.options[displayDropdown.selectedIndex].value;
+  var notes = document.getElementById('notes').value;
+
+  data.tangos[tango] = {
+      hiragana,
+      katakana,
+      display,
+      notes
+  };
+
+  chrome.storage.sync.set(data, function() {
+    // Update status to let user know the tangos were saved.
     var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
+    status.textContent = 'Tango saved.';
     setTimeout(function() {
       status.textContent = '';
     }, 750);
   });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
 function restore_options() {
-// Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
-  }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
+  chrome.storage.sync.get('tangos', function(tangos) {
+    data = tangos;
   });
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-save_options);
+document.getElementById('save').addEventListener('click', save_options);
